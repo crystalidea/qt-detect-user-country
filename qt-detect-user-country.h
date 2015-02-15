c64f92280a5a65d8ac30c2bc24b2b67503c2a83d
CrystalIDEA <support@crystalidea.com>
2015-02-15 15:48:43 +0300

    Actual source code, image update

---
+++ qt-detect-user-country.h
@@ -0,0 +1,9 @@
+#ifndef qt-detect-user-country_h__
+#define qt-detect-user-country_h__
+
+namespace LocalizationHelpers
+{
+	QLocale::Country detectUserCountry();
+}
+
+#endif // qt-detect-user-country_h__
